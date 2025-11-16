4cb7372c771f58d1765577dc4fac0eadaf3d329c
ceilidemo <demaraisceili@gmail.com>
2025-11-15 21:47:08 -0700

    change marked function

--- assets/js/md_HTML.js
+++ assets/js/md_HTML.js
@@ -1,3 +1,4 @@
+/* Load script that can parse md to html */
 const markedScript = document.createElement("script");
 markedScript.src = "https://cdn.jsdelivr.net/npm/marked/marked.min.js";
 document.head.appendChild(markedScript);
